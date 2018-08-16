de823e0354c67effd7df4d79d570763543521012
ployploy <chonnikan.t@kkumail.com>
2018-08-16 13:40:35 +0700

    [Added]:test struct to json

--- src/xml/xml2json_test.go
+++ src/xml/xml2json_test.go
@@ -1,6 +1,7 @@
 package xml
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"io/ioutil"
 	"testing"
@@ -11,8 +12,8 @@ type GetCountriesAvailable struct {
 }
 
 type CountryCode struct {
-	Code        string `xml:"Code"`
-	Description string `xml:"Description"`
+	Code        string `xml:"Code" json:"code"`
+	Description string `xml:"Description" json:"description"`
 }
 
 func Test_ConvertXML_Input_XML_Should_Be_Struct(t *testing.T) {
@@ -38,4 +39,22 @@ func Test_ConvertXML_Input_XML_Should_Be_Struct(t *testing.T) {
 		}
 	}
 
+}
+
+func Test_ToJson_Should_Be_Json(t *testing.T) {
+	expected := `{"CountryCode":[{"code":"Canada","description":"Canada"},{"code":"GreatBritain","description":"Great Britain and Wales"},{"code":"IrelandNorthern","description":"Northern Ireland"},{"code":"IrelandRepublicOf","description":"Republic of Ireland"},{"code":"Scotland","description":"Scotland"},{"code":"UnitedStates","description":"United States"}]}`
+	getCountriesAvailable := GetCountriesAvailable{
+		CountryCode: []CountryCode{
+			CountryCode{"Canada", "Canada"},
+			CountryCode{"GreatBritain", "Great Britain and Wales"},
+			CountryCode{"IrelandNorthern", "Northern Ireland"},
+			CountryCode{"IrelandRepublicOf", "Republic of Ireland"},
+			CountryCode{"Scotland", "Scotland"},
+			CountryCode{"UnitedStates", "United States"},
+		},
+	}
+	actual, _ := json.Marshal(getCountriesAvailable)
+	if expected != string(actual) {
+		t.Errorf("Expect %s but it got %s", expected, actual)
+	}
 }
