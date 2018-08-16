9dd190d5710b381c61c03d2dae565dcc1c71af93
ployploy <chonnikan.t@kkumail.com>
2018-08-16 13:55:15 +0700

    [Added]: แยก struct ของ json กับ xml ออกจากการ เพื่อให้แยกการทำงานที่ชัดเจอและไม่ผูกติดกัน

--- src/xml/xml2json_test.go
+++ src/xml/xml2json_test.go
@@ -7,15 +7,36 @@ import (
 	"testing"
 )
 
+type CountriesResponse struct {
+	Counties []Country `json:"counties"`
+}
+
+type Country struct {
+	Code        string `json:"code"`
+	Description string `json:"description"`
+}
+
 type GetCountriesAvailable struct {
 	CountryCode []CountryCode `xml:"Body>GetCountriesAvailableResponse>GetCountriesAvailableResult>CountryCode"`
 }
 
 type CountryCode struct {
-	Code        string `xml:"Code" json:"code"`
-	Description string `xml:"Description" json:"description"`
+	Code        string `xml:"Code"`
+	Description string `xml:"Description"`
 }
 
+func (g GetCountriesAvailable) ToJSON() CountriesResponse {
+	counties := make([]Country, len(g.CountryCode))
+	for index := range g.CountryCode {
+		counties[index] = Country{
+			Code:        g.CountryCode[index].Code,
+			Description: g.CountryCode[index].Description,
+		}
+	}
+	return CountriesResponse{
+		Counties: counties,
+	}
+}
 func Test_ConvertXML_Input_XML_Should_Be_Struct(t *testing.T) {
 	var actual GetCountriesAvailable
 	xmlFile, _ := ioutil.ReadFile("./reponse.xml")
@@ -42,7 +63,7 @@ func Test_ConvertXML_Input_XML_Should_Be_Struct(t *testing.T) {
 }
 
 func Test_ToJson_Should_Be_Json(t *testing.T) {
-	expected := `{"CountryCode":[{"code":"Canada","description":"Canada"},{"code":"GreatBritain","description":"Great Britain and Wales"},{"code":"IrelandNorthern","description":"Northern Ireland"},{"code":"IrelandRepublicOf","description":"Republic of Ireland"},{"code":"Scotland","description":"Scotland"},{"code":"UnitedStates","description":"United States"}]}`
+	expected := `{"counties":[{"code":"Canada","description":"Canada"},{"code":"GreatBritain","description":"Great Britain and Wales"},{"code":"IrelandNorthern","description":"Northern Ireland"},{"code":"IrelandRepublicOf","description":"Republic of Ireland"},{"code":"Scotland","description":"Scotland"},{"code":"UnitedStates","description":"United States"}]}`
 	getCountriesAvailable := GetCountriesAvailable{
 		CountryCode: []CountryCode{
 			CountryCode{"Canada", "Canada"},
@@ -53,7 +74,8 @@ func Test_ToJson_Should_Be_Json(t *testing.T) {
 			CountryCode{"UnitedStates", "United States"},
 		},
 	}
-	actual, _ := json.Marshal(getCountriesAvailable)
+	countiesResults := getCountriesAvailable.ToJSON()
+	actual, _ := json.Marshal(countiesResults)
 	if expected != string(actual) {
 		t.Errorf("Expect %s but it got %s", expected, actual)
 	}
